49e4b0bdca27000159f6d40a479f1ae5748a4a1c
Yuulis <yuulis2005@gmail.com>
2023-01-20 19:58:27 +0900

    Ready for "School_5F"

--- CreateHeatmap.py
+++ CreateHeatmap.py
@@ -4,11 +4,11 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-list = pd.read_csv("./resultData/School_3F_result_2023-01-19-22-50-03.csv").values.tolist()
+list = pd.read_csv("./resultData/School_4F_result_2023-01-20-18-48-32.csv").values.tolist()
 
 plt.figure(figsize=(15, 8), dpi=130)
 plt.axis("off")
-sns.heatmap(list, square=True, cmap='hot', vmin=0, vmax=2000)
+sns.heatmap(list, square=True, cmap='hot', vmin=0, vmax=3000)
 
 # plt.show()
-plt.savefig("./result_heatmaps/School_3F_result_2023-01-19-22-50-03.png")
+plt.savefig("./result_heatmaps/School_4F_result_2023-01-20-18-48-32.png")
